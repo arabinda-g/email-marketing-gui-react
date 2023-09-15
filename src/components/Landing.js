80a623794b2ebd02f36857478b4636ce2e63177c
Arabinda Ghosh <me@arabinda.me>
2023-09-16 05:23:30 +0530

    Public page changes.

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,10 +2,12 @@
 import React from 'react';
 
 function Landing() {
+  window.location.href = '/login';
+
   return (
     <div>
-      <h1>Welcome to My App</h1>
-      <p>This is the landing page.</p>
+      {/* <h1>Welcome to My App</h1>
+      <p>This is the landing page.</p> */}
     </div>
   );
 }
